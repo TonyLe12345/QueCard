64ae4d88d5f5624d79eb0e6820a23371900ac493
TONY <a1908861@adelaide.edu.au>
2025-06-27 03:39:18 +0930

    data

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,1 +1,10 @@
-import { defineStorage } from "@aws-amplify/backend";export const storage = defineStorage({  name: "amplifyNotesDrive",  access: (allow) => ({    "media/{entity_id}/*": [      allow.entity("identity").to(["read", "write", "delete"]),    ],  }),});
+import { defineStorage } from "@aws-amplify/backend";
+
+export const storage = defineStorage({
+  name: "amplifyNotesDrive",
+  access: (allow) => ({
+    "public/images/{identity.identityId}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
+});
